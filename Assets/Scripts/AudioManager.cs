28df460585f8c1a243c9c03f84fdbad3bf9f5540
tamirco2003 <tamir.cohen835@gmail.com>
2018-04-10 18:03:29 +0300

    Readded everything, hopefully nothing broke.

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -12,16 +12,38 @@ public class AudioManager : MonoBehaviour {
             sound.source.clip = sound.clip;
             sound.source.volume = sound.volume;
             sound.source.pitch = sound.pitch;
+            sound.source.loop = sound.loop;
         }
+    }
+
+    void Start() {
+        Play("SceneMusic");
     }
 
     public void Play(string name) {
         Sound sound = Array.Find(sounds, s => s.name == name);
+        if (sound == null) {
+            Debug.LogWarning("Sound " + name + " has not been found.");
+            return;
+        }
+        sound.source.Play();
+    }
+
+    public void PlayOnce(string name) {
+        Sound sound = Array.Find(sounds, s => s.name == name);
+        if (sound == null) {
+            Debug.LogWarning("Sound " + name + " has not been found.");
+            return;
+        }
         sound.source.PlayOneShot(sound.clip);
     }
 
     public Sound FindSound(string name) {
         Sound sound = Array.Find(sounds, s => s.name == name);
+        if (sound == null) {
+            Debug.LogWarning("Sound " + name + " has not been found.");
+            return null;
+        }
         return sound;
     }
 }
