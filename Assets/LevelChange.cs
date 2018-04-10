d4234bb6a0f04ab77e2e93ed1116f004313fee99
l3th4l <safalyaglobal@gmail.com>
2018-04-11 04:36:27 +0530

    pfft donee

---
+++ Assets/LevelChange.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelChange : MonoBehaviour 
+{
+    public int index;
+
+    void OnTriggerEnter( Collider kys)
+    {
+        if (kys.CompareTag("LevC"))
+            SceneManager.LoadScene(index);
+    }
+}
